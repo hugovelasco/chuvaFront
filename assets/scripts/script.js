b358c48db8322da29bc5fd41b276f5561c279656
@hugovelasco <hugo.c.velasco@gmail.com>
2022-04-18 21:32:11 -0300

    finishing the prototyte

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,9 +15,41 @@ function showMe() {
 
         beforeDots.style.display = "inline";
         afterDots.style.display = "none";
-        
+
         beforeDots.innerHTML = '<span id="before-dots" onclick="showMe()">... ver mais</span>';
         afterDots.innerHTML += beforeDots.innerHTML;
         console.log("else");
     }
 }
+
+let qsContainer = document.getElementById("qs-container");
+let standardSquareContainer = document.getElementById("idSquareContainer");
+let backgroudMessage = document.getElementById("sub-standard-square-background-msg");
+
+document.getElementById("addTopicBtn").addEventListener("click", () => {
+
+    qsContainer.style.display = "block";
+    standardSquareContainer.style.display = "none";
+    backgroudMessage.style.display = "none";
+
+});
+
+document.getElementById("qsEnviarBtn").addEventListener("click", () => {
+
+    qsContainer.style.display = "none";
+    standardSquareContainer.style.display = "block";
+    backgroudMessage.style.display = "block";
+
+});
+
+document.getElementById("idSubStandardSquare").addEventListener("click", () => { 
+
+    let authorsContainer = document.getElementById("authors-container");
+
+    if(authorsContainer.style.display === "" || authorsContainer.style.display === "none"){
+        authorsContainer.style.display = "block"
+    } else {
+        authorsContainer.style.display = "none"
+    }
+
+});
